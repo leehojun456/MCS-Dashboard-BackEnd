a183f37556de44fa12fd00afcc3c198ced1d4499
Lee Hojun <hppmm@naver.com>
2024-11-26 05:06:07 +0900

    init

---
+++ src/main/java/com/mooo/calb/mcsd/McsdApplication.java
@@ -0,0 +1,13 @@
+package com.mooo.calb.mcsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McsdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mooo/calb/mcsd/McsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mooo.calb.mcsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
